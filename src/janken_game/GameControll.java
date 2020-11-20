be54f1b5ccbb30eadb581307e6ff06497a5f3a5b
hinako <hinako@ndg-no-mbp>
2020-11-20 11:20:21 +0900

    eclipceアンインストールのため更新

--- src/janken_game/GameControll.java
+++ src/janken_game/GameControll.java
@@ -2,37 +2,41 @@ package janken_game;
 
 public class GameControll {
 	private Hand myHand;;
-    private Hand opponentHand;
-    private Rule rule;
+	private Hand opponentHand;
+	private Rule rule;
 
-    //コンストラクタ
-    public GameControll() {
-    	this.rule = new Rule();
-        this.myHand = new Hand();
-        this.opponentHand = new Hand();
-    }
+	// コンストラクタ
+	public GameControll() {
+		this.rule = new Rule();
+		this.myHand = new Hand();
+		this.opponentHand = new Hand();
+	}
 
-    //ゲームスタート
-    public void start() {
-    	System.out.print("3回勝負！");
-    	System.out.println("じゃんけんスタート！");
-    }
-    public void game() {
-    	myHand.setHand();
-    	opponentHand.setRandomHand();
-        rule.showHands(myHand, opponentHand);
-        rule.showResult(myHand, opponentHand);
-    }
-    public void judge() {
-    	System.out.print("あなたは" + rule.win + "回勝って");
-    	System.out.print(rule.draw + "回あいこで");
-    	System.out.println(rule.lose + "回負けました。");
-    }
+	// ゲームスタート
+	public void start() {
+		System.out.print("3回勝負！");
+		System.out.println("じゃんけんスタート！");
+	}
 
-    //ゲーム終了
-    public void end() {
-        System.out.println("ゲーム終了！");
+	public void game() {
+		myHand.setHand();
+		opponentHand.setRandomHand();
+		rule.showHands(myHand, opponentHand);
+		rule.showResult(myHand, opponentHand);
+	}
 
-    }
+	public void judge() {
+		System.out.println("☆最終結果☆");
+		System.out.println("----------------");
+		System.out.println("勝ち: " + rule.win + "回");
+		System.out.println("負け: " + rule.lose + "回");
+		System.out.println("あいこ: " + rule.draw + "回");
+	}
+
+	// ゲーム終了
+	public void end() {
+		System.out.println("ゲーム終了！");
+
+	}
 
 }
--- src/janken_game/Hand.java
+++ src/janken_game/Hand.java
@@ -6,44 +6,66 @@ import java.util.Scanner;
 
 public class Hand {
 	private int hand;
+	private int count = 0;
 
-	//手を入力で指定
+	// 手を入力で指定
 	public void setHand() {
-		System.out.println("何を出しますか？");
-        System.out.println("グー：1、チョキ：2、パー：3");
-
-        while(true) {
-        	try {
-        		Scanner scan = new Scanner(System.in);
-                this.hand = scan.nextInt();
-                if(hand == 0 || hand >= 4) {
-                	System.out.println("数値は1,2,3から選んでください");
-                	this.hand = scan.nextInt();
-                	//連続でできない　２回連続で正しくない数値を入れるとnullが返ってくる
-                }
-                break;
-        	}catch(InputMismatchException e) {
-        		System.out.println("数値で入力してください。");
-        	}
-        }
+		count = 0;
+		do {
+			System.out.println("何を出しますか？");
+			System.out.println("グー：1、チョキ：2、パー：3");
+		} while (!isNumber());
 	}
 
-	//手をランダムに設定
+	/**
+	 * 入力値が1か2か3であるか判定する
+	 * 1,2,3, 数値以外 ならfalseを返す
+	 * @return
+	 */
+	private boolean isNumber() {
+
+		if (count < 10) {// 10回以上間違えるようなら強制的に"グー"を設定する
+			try {
+				Scanner scan = new Scanner(System.in);
+				this.hand = scan.nextInt();
+				if (!(hand == 1 || hand == 2 || hand == 3)) {
+					System.out.println("数値は1,2,3から選んでください");
+					// 連続でできない ２回連続で正しくない数値を入れるとnullが返ってくる
+					count++;
+					return false;
+				}
+				return true;
+			} catch (InputMismatchException e) {
+				System.out.println("数値で入力してください。");
+				count++;
+				return false;
+			}
+		} else {
+			this.hand = 1;
+			return true;
+		}
+
+	}
+
+	// 手をランダムに設定
 	public void setRandomHand() {
 		Random random = new Random();
-		this.hand = random.nextInt(3)+1;
+		this.hand = random.nextInt(3) + 1;
 	}
 
-	//手の名称を返す
+	// 手の名称を返す
 	public String handName() {
 		String handName = null;
-		if(this.hand == 1) handName = "グー";
-        if(this.hand == 2) handName = "チョキ";
-        if(this.hand == 3) handName = "パー";
-        return handName;
+		if (this.hand == 1)
+			handName = "グー";
+		if (this.hand == 2)
+			handName = "チョキ";
+		if (this.hand == 3)
+			handName = "パー";
+		return handName;
 	}
 
-	//手の値を返す
+	// 手の値を返す
 	public int hand() {
 		return this.hand;
 	}
